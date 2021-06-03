0833204696bd07310483d096c3bec74482f39c3a
Salut <wujinyi@umich.edu>
2021-06-03 21:46:25 +0800

    update process board

--- src/ProcessIndex.elm
+++ src/ProcessIndex.elm
@@ -12,35 +12,38 @@ import Palette
 
 
 
-view model = Element.row
+view model =
+    [ todo
+    , inProcess
+    , done
+    ]
+    |> Element.row
         [ Element.width Element.fill
-         , Element.spacing 10
+        , Element.spacing 10
         , Element.centerX
+        ]
 
-        ]
-        [ todo
-        , inProcess
-        , done
-        ]
 
 header text=
-    Element.el
-                [ Element.width Element.fill
-                , Element.centerX
-                , Element.centerX
-                , Font.extraBold
-                , Font.size 28
-                , Font.color (Element.rgba255 0 0 0 0.7)
-                ]
-                (Element.text text)
+    Element.text text
+    |> Element.el
+         [ Element.width Element.fill
+         , Element.centerX
+         , Element.centerX
+         , Font.extraBold
+         , Font.size 28
+         , Font.color (Element.rgba255 0 0 0 0.7)
+         ]
 
 boardEl color=
     Element.column
     [ Element.width Element.fill
-                  ,Element.height <| Element.px 600
     , Element.spacing 30
+    , Element.height Element.fill
     , Element.padding 10
     , Element.Border.width 1
+    , Element.alignTop
+    , Element.scrollbars
     , Element.Background.color color
     , Element.Border.color (Element.rgba255 0 0 0 0.1)
     , Element.mouseOver
@@ -53,56 +56,54 @@ cardEl color a=
             , Element.height <| Element.px 200
             , Element.Background.color color
             , Element.padding 8
+            , Font.extraLight
+            , Font.color (Element.rgba255 0 0 0 0.7)
             , Element.Border.shadow
                                   { offset = ( 1, 2 )
                                   , blur = 1
                                   , size = 2
-                                  , color =
-                                          Element.rgb (238 / 255) (238 / 255) (238 / 255)
+                                  , color = Element.rgb (238 / 255) (238 / 255) (238 / 255)
                                   }
             , Element.mouseOver
                 [  Element.Border.shadow
                                                     { offset = ( 2, 4 )
                                                     , blur = 1
                                                     , size = 4
-                                                    , color =
-                                                            Element.rgb (238 / 255) (238 / 255) (238 / 255)
+                                                    , color = Element.rgb (238 / 255) (238 / 255) (238 / 255)
                                                     }]
 
             ]
             [a]
 
+todoCard text=
+    cardEl (Element.rgb255 255 255 255) (Element.text text)
+
 todo : Element msg
 todo =
     boardEl (Element.rgba255 240 242 250 0.3)
             [ header "To Do"
-            , todoCard1
-            , todoCard2]
+            , todoCard "Complete functions of this GSoC blog"
+            ]
 
-
-todoCard1 =
-    cardEl (Element.rgb255 255 255 255)
-        (Element.text ("To Do item"))
-
-todoCard2 =
-    cardEl (Element.rgb255 255 255 255)
-        (Element.text ("To Do item"))
+inProcessCard text=
+    cardEl (Element.rgba255 240 242 250 0.3) (Element.text text)
 
 inProcess : Element msg
 inProcess =
     boardEl (Element.rgb255 255 255 255)
         [ header "Doing"
-        , cardEl (Element.rgba255 240 242 250 0.3)
-                               (Element.text ("Done item"))
-        , cardEl (Element.rgba255 240 242 250 0.3)
-                                (Element.text ("Done item"))]
+        , inProcessCard "Clarify HTML fallback spec"
+        , inProcessCard "Write blog posts and github discussions"
+        ]
+
+doneCard text=
+    cardEl (Element.rgb255 255 255 255) (Element.text text)
 
 done : Element msg
 done =
     boardEl (Element.rgba255 240 242 250 0.3)
             [ header "Done"
-            , cardEl (Element.rgb255 255 255 255)
-                     (Element.text ("Done item"))
-            , cardEl (Element.rgb255 255 255 255)
-                      (Element.text ("Done item"))
+            , doneCard "Set up this GSoC blog by elm-pages and elm-ui"
+            , doneCard "Learn parsing technologies from elm-review and elm-syntax"
+            , doneCard "Wrap around my previous elm games by elm-pages and create-elm-app"
             ]
